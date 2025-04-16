5b84305a140715b19093a30fcb755d68a6acf2dd
Avner Gonçalves <avner.goncalves@consultor.redspark.io>
2025-04-16 10:24:26 -0300

    feat: adding fetch template action

--- src/adapters/tasks/template_tasks.py
+++ src/adapters/tasks/template_tasks.py
@@ -3,7 +3,10 @@ import logging
 
 from src.adapters.api.schemas.template.v1beta1.enums.action import ActionEnum
 from src.adapters.clients.spark_service_hub_client import SparkServiceHubClient
-from src.domain.services.application_register_service import ApplicationRegisterService
+from src.domain.services.application_fetch_template import FetchTemplateService
+from src.domain.services.applications.application_register_service import (
+    ApplicationRegisterService,
+)
 from src.infrastructure.celery import CustomBaseTask, celery_app
 
 logger = logging.getLogger("uvicorn")
@@ -17,7 +20,14 @@ def step_runner_task(steps, parameters, token):
     logger.info("Task starting...")
     outputs = {}
     for step in steps:
-        if step["action"] == ActionEnum.application_register:
+        if step["action"] == ActionEnum.fetch_template:
+            application_register_service = FetchTemplateService(
+                SparkServiceHubClient(token)
+            )
+            outputs[step["id"]] = asyncio.run(
+                application_register_service.handler(step["input"], parameters)
+            )
+        elif step["action"] == ActionEnum.application_register:
             application_register_service = ApplicationRegisterService(
                 SparkServiceHubClient(token)
             )
